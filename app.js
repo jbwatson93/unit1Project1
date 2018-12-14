d6c5988b9149d1f89208e883f03f32aec6af506d
JB Watson <jbwatson93@gmail.com>
2018-12-14 11:23:32 -0500

    working on js logic

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 var compSequence = [];
 let level = 1;
+ var on = false
+ var start = false;
+function generateSeq(){
+    
+        compSequence.push(Math.floor(Math.random() * 4) + 1)
+    
+}
 
+function lightButtons(){
+
+}
