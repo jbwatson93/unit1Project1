b6fbedc94fd8ff90bef3fcad8c118c3aa5610a67
JB Watson <jbwatson93@gmail.com>
2018-12-14 14:12:14 -0500

    working on js and dom

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ let level = 6;
  var on = false
  var start = false;
  var tempo = 2000;
- var buttons =[$("#blue"),$("#yellow"),$("#red",$("#green"))];
+ var buttons =[$("#blue"),$("#yellow"),$("#red"),$("#green")];
  var colors = ["blue","yellow","red","green"]
- var levelCount = 0
+//  var levelCount = 0
 function generateSeq(){
     
-        compSequence.push(Math.floor(Math.random() * 4) + 1)
+        compSequence.push(Math.floor(Math.random() * 4) )
     
 }
 generateSeq()
@@ -17,12 +17,28 @@ generateSeq()
 generateSeq()
 generateSeq()
 generateSeq()
+
+
+
 function lightButtons(){
+  
 for(let levelCount =0;levelCount < level;levelCount++){
+    function loopDelay(levelCount){
     setTimeout(function (){
+        
         console.log(buttons[compSequence[levelCount]])
-        buttons[compSequence[levelCount]].css("background","readial-gradient(white,"+colors[compSequence[levelCount]]+")")
-    }, tempo )
+        buttons[compSequence[levelCount]].fadeOut(50).fadeIn(60)
+    }, tempo * levelCount)
+} loopDelay(levelCount)
 }
+
 }
-lightButtons()
+
+// $("#on").on("click", onOff())
+function onOff(){
+    $("#on").css("justify-content", "flex-start")
+   setTimeout(function(){
+    lightButtons()
+   }, 3000)
+    ;
+}
