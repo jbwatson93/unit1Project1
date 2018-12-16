6119cfb5e8049db492ec0ff3c98dfbbbdb91ab13
JB Watson <jbwatson93@gmail.com>
2018-12-16 18:27:26 -0500

    working on sounds

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ let level = 6;
  var tempo = 2000;
  var buttons =[$("#blue"),$("#yellow"),$("#red"),$("#green")];
  var colors = ["blue","yellow","red","green"]
-//  var levelCount = 0
+ var levelCount = 0
+ var sound = [$("#sound1"),$("#sound2"),$("#sound3"),$("#sound4")]
+ 
 function generateSeq(){
     
         compSequence.push(Math.floor(Math.random() * 4) )
@@ -25,7 +27,9 @@ function lightButtons(){
 for(let levelCount =0;levelCount < level;levelCount++){
     function loopDelay(levelCount){
     setTimeout(function (){
-        
+        function (){
+            sound[compSequence[levelCount]].play()
+        };
         console.log(buttons[compSequence[levelCount]])
         buttons[compSequence[levelCount]].fadeOut(50).fadeIn(60)
     }, tempo * levelCount)
@@ -42,3 +46,64 @@ function onOff(){
    }, 3000)
     ;
 }
+function lightOnTouch(i){
+    buttons[i].fadeOut(50).fadeIn(60)
+}
+function checkBlue(){
+    if(compSequence[levelCount] == 0){
+        levelCount++
+        generateSeq()
+    }
+    else{
+        levelCount --;
+
+    }
+}
+function checkYellow(){
+    if(compSequence[levelCount] == 1){
+        levelCount++
+        generateSeq()
+    }
+    else{
+        levelCount --;
+        
+    }
+}
+function checkRed(){
+    if(compSequence[levelCount] == 2){
+        levelCount++
+        generateSeq()
+    }
+    else{
+        levelCount --;
+        
+    }
+}
+function checkgreen(){
+    if(compSequence[levelCount] == 3){
+        levelCount++
+        generateSeq()
+    }
+    else{
+        levelCount --;
+        
+    }
+}
+
+$("#blue").on("click", function (){
+    lightOnTouch(0);
+
+})
+$("#yellow").on("click", function (){
+    lightOnTouch(1);
+    
+})
+$("#red").on("click", function (){
+    lightOnTouch(2);
+    
+})
+
+$("#green").on("click", function (){
+    lightOnTouch(3);
+    
+})
