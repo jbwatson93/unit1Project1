91ccd6e3dfcb0fa50e8803b156c60a7c54fc7e3e
JB Watson <jbwatson93@gmail.com>
2018-12-16 22:34:53 -0500

    working on the ceck button statements

--- app.js
+++ app.js
@@ -1,78 +1,126 @@
 var compSequence = [];
-let level = 6;
+let level = compSequence.length;
  var on = false
  var start = false;
  var tempo = 2000;
  var buttons =[$("#blue"),$("#yellow"),$("#red"),$("#green")];
  var colors = ["blue","yellow","red","green"]
  var levelCount = 0
- var sound = [$("#sound1"),$("#sound2"),$("#sound3"),$("#sound4")]
- 
+ var sound = [document.getElementById("sound1"),document.getElementById("sound2"),document.getElementById("sound3"),document.getElementById("sound4")]
+ var compTurn = true
+
+//-----------=========------=====-----======------------
+//----------function tp generate and new addition to sequence
+//-------------------================------------------
 function generateSeq(){
     
         compSequence.push(Math.floor(Math.random() * 4) )
     
 }
 generateSeq()
-generateSeq()
-generateSeq()
-generateSeq()
-generateSeq()
-generateSeq()
 
 
 
+function playSounds(i){
+sound[i].play()
+}
+//-------=========--------========-------------
+// function to play back sequence +1///////////
+//===========================================
 function lightButtons(){
+    if(compTurn == true){
+    
   
-for(let levelCount =0;levelCount < level;levelCount++){
+for( levelCount =0;levelCount < compSequence.length;levelCount++){
     function loopDelay(levelCount){
     setTimeout(function (){
-        function (){
-            sound[compSequence[levelCount]].play()
-        };
+     sound[compSequence[levelCount]].play();
         console.log(buttons[compSequence[levelCount]])
         buttons[compSequence[levelCount]].fadeOut(50).fadeIn(60)
     }, tempo * levelCount)
 } loopDelay(levelCount)
-}
 
 }
+levelCount = 0
 
+compTurn = false}
+
+else{}
+}
+// on and off switch---------------------
+//=========================================
 // $("#on").on("click", onOff())
 function onOff(){
+    if(on == true){
+        $("#on").css("justify-content", "flex-end");
+        on =false
+        location.reload()
+    }
+    else{
+        on = true
     $("#on").css("justify-content", "flex-start")
    setTimeout(function(){
     lightButtons()
    }, 3000)
-    ;
+}
 }
 function lightOnTouch(i){
     buttons[i].fadeOut(50).fadeIn(60)
 }
 function checkBlue(){
     if(compSequence[levelCount] == 0){
-        levelCount++
-        generateSeq()
+       if(levelCount = compSequence.length){
+        $("#level").text(level)
+           levelCount = 0
+           generateSeq()
+           compTurn = true;
+           lightButtons()
+           
+       }
+       else{
+           levelcount++
+       }   
     }
     else{
-        levelCount --;
+        
 
     }
 }
 function checkYellow(){
     if(compSequence[levelCount] == 1){
-        levelCount++
-        generateSeq()
+        if(levelCount = compSequence.length){
+            $("#level").text(level)
+            levelCount = 0
+            generateSeq()
+            compTurn = true;
+            lightButtons()
+            
+        }
+        else{
+            levelcount++
+        }   
+        
+        
     }
     else{
-        levelCount --;
+       level--
         
     }
 }
 function checkRed(){
     if(compSequence[levelCount] == 2){
-        levelCount++
-        generateSeq()
+        if(levelCount = compSequence.length){
+            $("#level").text(level)
+            levelCount = 0
+            generateSeq()
+            compTurn = true;
+            lightButtons()
+            
+        }
+        else{
+            levelcount++
+        }   
+       
     }
     else{
         levelCount --;
@@ -81,8 +129,17 @@ function checkRed(){
 }
 function checkgreen(){
     if(compSequence[levelCount] == 3){
-        levelCount++
-        generateSeq()
+        if(levelCount = compSequence.length){
+            $("#level").text(level)
+            levelCount = 0
+            generateSeq()
+            compTurn = true;
+            lightButtons()
+            
+        }
+        else{
+            levelcount++
+        }   
     }
     else{
         levelCount --;
@@ -91,19 +148,44 @@ function checkgreen(){
 }
 
 $("#blue").on("click", function (){
+   if(compTurn == true){
+    } 
+   else{
     lightOnTouch(0);
+playSounds(0)
+checkBlue();
 
+
+   }
 })
 $("#yellow").on("click", function (){
+    if(compTurn == true){
+    } 
+   else{
     lightOnTouch(1);
+    playSounds(1)
+    checkYellow();
     
+   }
 })
 $("#red").on("click", function (){
+    if(compTurn == true){
+    } 
+   else{
     lightOnTouch(2);
+    playSounds(2)
+    checkRed();
     
+   }
 })
 
 $("#green").on("click", function (){
+    if(compTurn == true){
+    } 
+   else{
     lightOnTouch(3);
+    playSounds(3);
+    checkgreen();
     
+   }
 })
